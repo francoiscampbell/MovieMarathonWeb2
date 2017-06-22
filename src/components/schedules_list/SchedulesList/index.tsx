f3d9cae76d46b2f49e5c245afb9447a2b86d1efd
Francois Campbell <campbell.francois@gmail.com>
2017-06-21 21:37:16 -0400

    removed nginx config because variables don't work in rewrite replacements. added image fetching

--- src/components/schedules_list/SchedulesList/index.tsx
+++ src/components/schedules_list/SchedulesList/index.tsx
@@ -1,23 +1,33 @@
 import * as Immutable from 'immutable'
 import * as React from 'react'
-import {Schedule} from "../../../data_model/Movie"
+import axios from 'axios'
+import {
+    Movie,
+    Schedule
+} from "../../../data_model/Movie"
 
 import * as styles from './styles.scss'
+import {generateSchedules} from "../../../scheduling/ScheduleGenerator"
 
 interface SchedulesListProps {
     schedules: Immutable.List<Schedule>
 }
 
 interface SchedulesListState {
-    imageUrls: Immutable.List<string>
+    imageUrls: Immutable.Map<Movie, string>,
+    imageUrlsToFetch: number
 }
 
 export default class SchedulesList extends React.PureComponent<SchedulesListProps, SchedulesListState> {
     state = {
-        imageUrls: Immutable.List<string>().setSize(this.props.schedules.size).set(0, '/tmdbimg/t/p/w154/ewVHnq4lUiovxBCu64qxq5bT2lu.jpg')
+        imageUrls: Immutable.Map<Movie, string>(),
+        imageUrlsToFetch: -1
     }
 
     render() {
+        if (this.state.imageUrlsToFetch === -1) {
+            this.fetchImageUrls()
+        }
         const items = this.props.schedules.map((schedule, index) => {
             const movies = schedule.get('movies').map((movie, index) => {
                 const delay = schedule.getIn(['delays', index])
@@ -27,7 +37,7 @@ export default class SchedulesList extends React.PureComponent<SchedulesListProp
                     <div>
                         <img
                             className={styles.moviePoster}
-                            src={this.state.imageUrls.get(index)}
+                            src={this.state.imageUrls.get(movie)}
                         />
                         <span className={styles.movieTitle}>{movie.get('title')}</span>
                         <span className={styles.showtime}>{movie.get('showtime').toString()}</span>
@@ -48,7 +58,39 @@ export default class SchedulesList extends React.PureComponent<SchedulesListProp
             <div>
                 <h1>Generated {this.props.schedules.size} {colon}</h1>
                 {items}
+                <span>
+                    Movie posters from <a href="https://www.themoviedb.org">TMDb</a>
+                </span>
             </div>
         )
+    }
+
+    fetchImageUrls() {
+        const movies = this.props.schedules.first().get('movies')
+        this.setState({imageUrlsToFetch: movies.size})
+        movies.forEach(movie => {
+            const config = {
+                params: {
+                    query: movie.get('title'),
+                    language: movie.get('titleLang'),
+                    year: movie.get('releaseYear'),
+                }
+            }
+            axios.get('/tmdb/3/search/movie', config)
+                .then(resp => {
+                    if (resp.data.results[0]) {
+                        const imageUrl = `/tmdbimg/t/p/w154/${resp.data.results[0].poster_path}`
+                        this.setState({
+                            imageUrls: this.state.imageUrls.set(movie, imageUrl),
+                            imageUrlsToFetch: this.state.imageUrlsToFetch - 1
+                        })
+                    } else {
+                        this.setState({
+                            imageUrlsToFetch: this.state.imageUrlsToFetch - 1
+                        })
+                    }
+                })
+                .catch(console.dir)
+        })
     }
 }
