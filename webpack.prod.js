06e77d49da7515bf723516340cef321d0e630ce3
Francois Campbell <campbell.francois@gmail.com>
2017-06-30 21:47:54 -0400

    updated prod build script

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 
 const defaultConfig = require('./webpack.config');
 defaultConfig.devtool = 'source-map';
+defaultConfig.output.filename = 'bundle-prod.js';
 defaultConfig.plugins = defaultConfig.plugins || [];
 defaultConfig.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
