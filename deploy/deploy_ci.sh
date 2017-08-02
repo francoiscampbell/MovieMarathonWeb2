49718628c0b58448c55ce7936f692cf4c0afdcf1
Francois Campbell <campbell.francois@gmail.com>
2017-08-02 08:39:00 -0400

    fix deploy script for ci?

--- deploy/deploy_ci.sh
+++ deploy/deploy_ci.sh
@@ -3,6 +3,7 @@
 TAG='gcr.io/moviemarathon-1495982743507/moviemarathon'
 KEY_FILE=${HOME}/gcp-key.json
 echo ${GOOGLE_AUTH} | base64 -i --decode > ${KEY_FILE}
+echo ${NGINX_CONF} | base64 -i --decode > deploy/default.conf
 docker build -t ${TAG} -f deploy/Dockerfile-prod deploy
 gcloud auth activate-service-account --key-file ${KEY_FILE}
 gcloud docker -- push ${TAG}
