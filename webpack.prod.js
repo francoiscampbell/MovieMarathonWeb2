e41878b10e30b3fc4b735602ff34d7c80ddc7a08
Francois Campbell <campbell.francois@gmail.com>
2017-08-05 11:14:04 -0400

    test-gh-pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const webpack = require('webpack');
 
 const defaultConfig = require('./webpack.config');
 defaultConfig.devtool = 'source-map';
-defaultConfig.output.filename = 'bundle-prod.js';
+defaultConfig.output.filename = 'bundle.js';
+defaultConfig.output.path = path.resolve('./docs/');
 defaultConfig.plugins = defaultConfig.plugins || [];
 defaultConfig.plugins.push(
     new webpack.optimize.UglifyJsPlugin({
