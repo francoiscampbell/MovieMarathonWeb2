aa61f1e0a7a033bc7df12b2b9f2c9f56aa3fb2c5
Francois Campbell <campbell.francois@gmail.com>
2017-09-04 10:24:34 -0400

    remove symlink

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 
-const devContentBase = path.resolve('./dist-dev/')
+const devContentBase = path.resolve('./docs-dev/')
 module.exports = {
     entry: [
         './src/index.js'
